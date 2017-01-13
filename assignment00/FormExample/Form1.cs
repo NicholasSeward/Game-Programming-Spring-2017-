25c41ada220fccbb6fc05a6d0a7b9f46c7421895
nseward6 <nseward6@gatech.edu>
2017-01-13 14:17:30 -0600

    added assignment00 starter code

---
+++ assignment00/FormExample/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FormExample
+{
+    public partial class Form1 : Form
+    {
+
+        int x;
+        int y;
+        int cellSize;
+        int margin=10;
+        int row = -1;
+        int col = -1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            cellSize = (Math.Min(ClientSize.Width, ClientSize.Height) - 2 * margin)/3;
+            if(ClientSize.Width> ClientSize.Height)
+            {
+                x = (ClientSize.Width - 3 * cellSize) / 2;
+                y = margin;
+            }
+            else
+            {
+                x = margin;
+                y= (ClientSize.Height - 3 * cellSize) / 2;
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+
+            base.OnResize(e);
+            UpdateSize();
+            Refresh();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            col = (int)Math.Floor((e.X - x)*1.0 / cellSize);
+            row = (int)Math.Floor((e.Y - y)*1.0 / cellSize);
+            Refresh();
+            //base.OnMouseDown(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            //base.OnPaint(e);
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    Rectangle rect = new Rectangle(x + i * cellSize, y + j * cellSize, cellSize, cellSize);
+                    if (i == col && j == row) e.Graphics.FillRectangle(Brushes.Yellow, rect);
+                    e.Graphics.DrawRectangle(Pens.Chocolate,rect);
+                    System.Drawing.Font font = new System.Drawing.Font("Ubuntu", cellSize*3 * 72 / 96 / 4);
+                    e.Graphics.DrawString("X", font, Brushes.Black, x + cellSize / 8, y + cellSize / 8);
+                }
+        }
+
+    }
+    
+}
---
+++ assignment00/FormExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FormExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FormExample")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4c4a00f-5d11-4010-b471-d3d7daaf0b9c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
